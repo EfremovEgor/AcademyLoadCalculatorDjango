2bfff160620522dc4f3de998b037c4ec66a72693
Efremov Egor <efremovegor936@gmail.com>
2023-10-13 14:57:27 +0300

    validators

--- src/website/validators.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ValidationError
-
-
-def validate_file_extension(value):
-    if not value.name.endswith(".json"):
-        raise ValidationError("Only .json file is accepted")
